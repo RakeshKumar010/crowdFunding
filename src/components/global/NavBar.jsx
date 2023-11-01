0e8a20d1611af69f62c425372c8e58592c780419
Rakesh Kumar <raksehkumar748844@gmail.com>
2023-11-01 09:26:39 +0530

    main sectect almost complate

--- src/components/global/NavBar.jsx
+++ src/components/global/NavBar.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import {PiUserCircleDuotone} from 'react-icons/pi'
+import {BiSolidDownArrow} from 'react-icons/bi'
 
 const NavBar = () => {
   const[show,setShow]=useState(true)
+  const [logStatus,setLogStatus]=useState(false)
+  const [userNav,setUserNav]=useState(false)
+  useEffect(()=>{
+    let data = localStorage.getItem('data')
+    console.log(data);
+    if(data){
+      setLogStatus(true)
+    }
+  })
   const showFun=()=>{
     setShow(!show)
   }
+  const logoutFun= ()=>{
+    // localStorage.clear()
+
+  }
   return (
-    <>
+    <>{!logStatus?
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to={"/"} className="flex items-center">
@@ -24,10 +39,19 @@ const NavBar = () => {
           <Link to={"./sigupfund"}>
             <button
               type="button"
-              className="text-white bg-teal-400 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-teal-400 dark:hover:bg-teal-500 dark:focus:ring-teal-700 hidden lg:block"
+              className="text-white  bg-teal-400 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 lg:mr-2 md:mr-0 dark:bg-teal-400 dark:hover:bg-teal-500 dark:focus:ring-teal-700 hidden lg:block"
             >
               Start a Fundraiser
             </button>
+            </Link>
+            <Link to={'/login'}>
+            <button
+              type="button"
+              className="text-black  hover:bg-teal-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-teal-400 dark:hover:bg-teal-500 dark:focus:ring-teal-700 hidden lg:block"
+            >
+            Sign In
+            </button>
+              
             </Link>
             <button
               onClick={showFun}
@@ -94,6 +118,141 @@ const NavBar = () => {
                   Stories
                 </Link>
               </li>
+             
+              <li>
+
+                <Link to={"./login"}>
+                  <button
+                    type="button"
+                    className="text-white bg-teal-400 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-teal-400 dark:hover:bg-teal-500 dark:focus:ring-teal-700 lg:hidden"
+                  >
+                  SignIn
+                  </button>
+                </Link>
+                <Link to={"./sigupfund"}>
+                  <button
+                    type="button"
+                    className="text-white bg-teal-400 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-teal-400 dark:hover:bg-teal-500 dark:focus:ring-teal-700 lg:hidden"
+                  >
+                    Start a Fundraiser
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      :
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to={"/"} className="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              MadadKaro
+            </span>
+          </Link>
+          <div className="flex md:order-2 ">
+          <Link to={"./sigupfund"}>
+            <button
+              type="button"
+              className="text-white  bg-teal-400 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 lg:mr-2 md:mr-0 dark:bg-teal-400 dark:hover:bg-teal-500 dark:focus:ring-teal-700 hidden lg:block"
+            >
+              Start a Fundraiser
+            </button>
+            </Link>
+            <div className="hidden  lg:block ">
+              <div className="active:bg-gray-400 flex w-fit items-center hover:cursor-pointer rounded-full" onClick={()=>{
+                setUserNav(!userNav)
+              }}>
+             <PiUserCircleDuotone className="text-4xl text-gray-600"/>
+             <BiSolidDownArrow className="text-xs"/>
+             </div>
+             <ul className={userNav?"shadow-md border-2 rounded-md  absolute [&>li]:p-2  backdrop-blur-sm":"hidden"}>
+              <li className="hover:bg-teal-400 hover:text-white hover:cursor-pointer rounded-t-md">Profile</li>
+              <li className="hover:bg-teal-400 hover:text-white hover:cursor-pointer ">My Fundraiser</li>
+              <li className="hover:bg-teal-400 hover:text-white hover:cursor-pointer ">My Donations</li>
+              <li className="hover:bg-teal-400 hover:text-white hover:cursor-pointer rounded-b-md" onClick={logoutFun}>Logout</li>
+             </ul>
+            </div>
+            {/* <Link to={'/login'}>
+            <button
+              type="button"
+              className="text-black  hover:bg-teal-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-teal-400 dark:hover:bg-teal-500 dark:focus:ring-teal-700 hidden lg:block"
+            >
+            Sign In
+            </button>
+              
+            </Link> */}
+            <button
+              onClick={showFun}
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className={show?"items-center justify-between hidden w-full md:flex md:w-auto md:order-1":"items-center justify-between  w-full md:flex md:w-auto md:order-1"}
+            id="navbar-cta"
+            
+          >
+            <ul className="flex bg-gray-700/30  flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li oncli>
+                <Link
+                  to={"./"}
+                  className="block py-2 pl-3 pr-4 text-gray-900  rounded md:bg-transparent hover:bg-gray-100 md:text-teal-400 md:p-0 md:dark:text-teal-400"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"./brosfundraiser"}
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Browse Fundraisers
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"./howitw"}
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  How It Works
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"./stories"}
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Stories
+                </Link>
+              </li>
+            
               <li>
 
                 <Link to={"./sigupfund"}>
@@ -108,7 +267,7 @@ const NavBar = () => {
             </ul>
           </div>
         </div>
-      </nav>
+      </nav>}
     </>
   );
 };
--- src/components/main/login/LoginUser.jsx
+++ src/components/main/login/LoginUser.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { CiUser, CiMail, CiLock, CiPhone } from "react-icons/ci";
+
 const LoginUser = () => {
+  const[email,setEmail]=useState()
+  const[pass,setPass]=useState()
+const navigater =useNavigate()
+
+  const changeFun =(e)=>{
+const{value,name}=e.target;
+  if(name=='email'){
+    setEmail(value)
+  }
+  if(name=='pass'){
+    setPass(value)
+  }
+  }
+  const submitFun =(e)=>{
+    e.preventDefault()
+    let data={
+      'eamil':email,
+    'pass':pass
+    }
+
+    localStorage.setItem('data',JSON.stringify(data))
+    navigater('/userProfile')
+
+  }
   return (
     <div
       className="flex justify-center bg-white items-center"
@@ -10,22 +35,27 @@ const LoginUser = () => {
       <div className="w-11/12 rounded-md my-2 flex flex-col  relative  items-center shadow-teal-400 shadow-lg border-2 py-4  lg:w-96">
         <h1 className="text-xl font-bold mb-3">LogIn</h1>
 
-        <form className=" w-full px-5">
+        <form className=" w-full px-5" onSubmit={submitFun}>
+   
           <div className="flex justify-between  border-b-2 w-full mb-3 items-center pr-2 ">
             <input
+            name="email"
               type="email"
               placeholder="Email Address *"
               required
               className="border-0  w-11/12"
+              onChange={changeFun}
             />
             <CiMail />
           </div>
           <div className="flex justify-between  border-b-2 w-full items-center pr-2 mb-24">
             <input
+            name="pass"
               type="password"
               placeholder="Creaate a Password*"
               required
               className="border-0  w-11/12"
+              onChange={changeFun}
             />
             <CiLock />
           </div>
@@ -37,12 +67,12 @@ const LoginUser = () => {
               Want to start a fundraiser? Click here
               </Link>
             </p>
-            <Link to={"/userProfile"}>
+            {/* <Link to={"/userProfile"}> */}
               {" "}
               <button className="bg-teal-400 w-full p-3 rounded-b-md text-white">
                 Next
               </button>
-            </Link>
+            {/* </Link> */}
           </div>
         </form>
       </div>
