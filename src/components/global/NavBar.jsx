c466cf567cd301d3cd0b8a9770ccd31b0bd68837
Rakesh Kumar <raksehkumar748844@gmail.com>
2023-11-01 07:16:55 +0530

    fix nav issue

--- src/components/global/NavBar.jsx
+++ src/components/global/NavBar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const[show,setShow]=useState(true)
+  const showFun=()=>{
+    setShow(!show)
+  }
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -26,6 +30,7 @@ const NavBar = () => {
             </button>
             </Link>
             <button
+              onClick={showFun}
               data-collapse-toggle="navbar-cta"
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -51,11 +56,12 @@ const NavBar = () => {
             </button>
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={show?"items-center justify-between hidden w-full md:flex md:w-auto md:order-1":"items-center justify-between  w-full md:flex md:w-auto md:order-1"}
             id="navbar-cta"
+            
           >
             <ul className="flex bg-gray-700/30  flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
+              <li oncli>
                 <Link
                   to={"./"}
                   className="block py-2 pl-3 pr-4 text-gray-900  rounded md:bg-transparent hover:bg-gray-100 md:text-teal-400 md:p-0 md:dark:text-teal-400"
@@ -86,14 +92,6 @@ const NavBar = () => {
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Stories
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to={"./about"}
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  About
                 </Link>
               </li>
               <li>
